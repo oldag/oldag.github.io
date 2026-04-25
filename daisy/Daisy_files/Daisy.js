dddd8238c81d8fac78d98f082e09ef350132e3ae
Darren L. Oldag <oldag@leftcheek.com>
2026-04-24 20:32:08 -0500

    initial drop of oldag.net

---
+++ daisy/Daisy_files/Daisy.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.4 local-build-20141218
+
+function createMediaStream_id1()
+{return IWCreatePhotocast("http://www.oldag.net/daisy/dog/Daisy_files/rss.xml",true);}
+function initializeMediaStream_id1()
+{createMediaStream_id1().load('http://www.oldag.net/daisy/dog',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id1',{pageIndex:0}));});}
+function layoutMediaGrid_id1(range)
+{createMediaStream_id1().load('http://www.oldag.net/daisy/dog',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id1',new IWPhotoGridLayout(3,new IWSize(165,165),new IWSize(165,0),new IWSize(204,180),27,27,0,new IWSize(2,2)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,{backgroundColor:'rgb(0, 0, 0)',reflectionHeight:100,reflectionOffset:2,captionHeight:100,fullScreen:0,transitionIndex:2},'Media/slideshow.html','widget1','widget2','widget3')});}
+function relayoutMediaGrid_id1(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id1(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id1(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Daisy_files/DaisyMoz.css')
+NotificationCenter.addObserver(null,relayoutMediaGrid_id1,'RangeChanged','id1')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');initializeMediaStream_id1()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
